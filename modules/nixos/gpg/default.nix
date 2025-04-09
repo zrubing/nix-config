341de317e19206fb854a0a645140bf63f5baccae
zrubing <rubingem@gmail.com>
2025-04-09 23:06:25 +0800

    Update gpg conf

--- modules/nixos/gpg/default.nix
+++ modules/nixos/gpg/default.nix
@@ -10,5 +10,9 @@
     pinentryPackage = pkgs.pinentry-qt;
     enableSSHSupport = false;
     settings.default-cache-ttl = 4 * 60 * 60; # 4 hours
+
+    settings = {
+      no-allow-external-cache = "";
+    };
   };
 }
