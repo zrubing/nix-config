b30b7441add27ccdad302190c7978ab04be6828b
zrubing <rubingem@gmail.com>
2025-04-15 12:11:13 +0800

    Add tree-sitter-libs

--- modules/home/emacs/tree-sitter-libs.nix
+++ modules/home/emacs/tree-sitter-libs.nix
@@ -16,6 +16,24 @@ in
     xdg.configFile = {
       "tree-sitter-libs/libtree-sitter-java.so".source =
         "${pkgs.tree-sitter-grammars.tree-sitter-java.outPath}/parser";
+      "tree-sitter-libs/libtree-sitter-vue.so".source =
+        "${pkgs.tree-sitter-grammars.tree-sitter-vue.outPath}/parser";
+      "tree-sitter-libs/libtree-sitter-typescript.so".source =
+        "${pkgs.tree-sitter-grammars.tree-sitter-typescript.outPath}/parser";
+      "tree-sitter-libs/libtree-sitter-tsx.so".source =
+        "${pkgs.tree-sitter-grammars.tree-sitter-tsx.outPath}/parser";
+      "tree-sitter-libs/libtree-sitter-javascript.so".source =
+        "${pkgs.tree-sitter-grammars.tree-sitter-javascript.outPath}/parser";
+      "tree-sitter-libs/libtree-sitter-css.so".source =
+        "${pkgs.tree-sitter-grammars.tree-sitter-css.outPath}/parser";
+      "tree-sitter-libs/libtree-sitter-scss.so".source =
+        "${pkgs.tree-sitter-grammars.tree-sitter-scss.outPath}/parser";
+      "tree-sitter-libs/libtree-sitter-markdown.so".source =
+        "${pkgs.tree-sitter-grammars.tree-sitter-markdown.outPath}/parser";
+      "tree-sitter-libs/libtree-sitter-html.so".source =
+        "${pkgs.tree-sitter-grammars.tree-sitter-html.outPath}/parser";
+      "tree-sitter-libs/libtree-sitter-python.so".source =
+        "${pkgs.tree-sitter-grammars.tree-sitter-python.outPath}/parser";
 
     };
   };
