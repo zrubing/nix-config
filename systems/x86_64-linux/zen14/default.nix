5000b8835438eaf8cd55f2a21ce7c8ce52d3fe45
zrubing <rubingem@gmail.com>
2025-04-18 14:37:06 +0800

    Add zen14 conf

---
+++ systems/x86_64-linux/zen14/default.nix
@@ -0,0 +1,66 @@
+{
+  config,
+  inputs,
+  lib,
+  pkgs,
+  system,
+  namespace,
+  ...
+}:
+{
+  snowfallorg.users.jojo = { };
+  snowfallorg.users.hiar = { };
+
+  time.timeZone = "Asia/Shanghai";
+
+  imports = [ ./hardware.nix ];
+
+  system.stateVersion = "24.11";
+
+  users.mutableUsers = true;
+  users.users = {
+
+    jojo = {
+      isNormalUser = true;
+      group = "users";
+      extraGroups = [
+        "wheel"
+        "networkmanager"
+        "docker"
+        "podman"
+      ];
+      initialPassword = "test";
+    };
+
+    hiar = {
+      isNormalUser = true;
+      group = "users";
+      extraGroups = [
+        "wheel"
+        "networkmanager"
+      ];
+      initialPassword = "test";
+    };
+  };
+
+  modules.secrets.desktop.enable = true;
+
+  ${namespace} = {
+    user.name = "jojo";
+    networking.wifi.enable = true;
+    #builder.enable = true;
+    desktop = {
+      #kde.enable = true;
+      niri.enable = true;
+    };
+
+    #dae.enable = true;
+    miho.enable = true;
+    desktop-programs.enable = true;
+
+    restic.enable = true;
+  };
+
+  networking.networkmanager.enable = true;
+
+}
