87ecbff8d220bf60cb5e0ca41e326e089cb13451
zrubing <rubingem@gmail.com>
2025-06-27 00:21:08 +0800

    update zsh config

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -58,69 +58,69 @@ in
       path = "${config.xdg.dataHome}/zsh_history";
     };
 
-    initExtra = ''
+    initContent = ''
       # search history based on what's typed in the prompt
-      autoload -U history-search-end
-      zle -N history-beginning-search-backward-end history-search-end
-      zle -N history-beginning-search-forward-end history-search-end
-      bindkey "^[OA" history-beginning-search-backward-end
-      bindkey "^[OB" history-beginning-search-forward-end
+            autoload -U history-search-end
+            zle -N history-beginning-search-backward-end history-search-end
+            zle -N history-beginning-search-forward-end history-search-end
+            bindkey "^[OA" history-beginning-search-backward-end
+            bindkey "^[OB" history-beginning-search-forward-end
 
-      # C-right / C-left for word skips
-      bindkey "^[[1;5C" forward-word
-      bindkey "^[[1;5D" backward-word
+            # C-right / C-left for word skips
+            bindkey "^[[1;5C" forward-word
+            bindkey "^[[1;5D" backward-word
 
-      # C-Backspace / C-Delete for word deletions
-      bindkey "^[[3;5~" forward-kill-word
-      bindkey "^H" backward-kill-word
+            # C-Backspace / C-Delete for word deletions
+            bindkey "^[[3;5~" forward-kill-word
+            bindkey "^H" backward-kill-word
 
-      # Home/End
-      bindkey "^[[OH" beginning-of-line
-      bindkey "^[[OF" end-of-line
+            # Home/End
+            bindkey "^[[OH" beginning-of-line
+            bindkey "^[[OF" end-of-line
 
 
-      # history search substring
-      bindkey '^[[A' history-substring-search-up
-      bindkey '^[[B' history-substring-search-down
+            # history search substring
+            bindkey '^[[A' history-substring-search-up
+            bindkey '^[[B' history-substring-search-down
 
-      # open commands in $EDITOR with C-e
-      # autoload -z edit-command-line
-      # zle -N edit-command-line
-      # bindkey "^e" edit-command-line
+            # open commands in $EDITOR with C-e
+            # autoload -z edit-command-line
+            # zle -N edit-command-line
+            # bindkey "^e" edit-command-line
 
-      # case insensitive tab completion
-      zstyle ':completion:*' completer _complete _ignored _approximate
-      zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
-      zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
-      zstyle ':completion:*' menu select
-      zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
-      zstyle ':completion:*' verbose true
+            # case insensitive tab completion
+            zstyle ':completion:*' completer _complete _ignored _approximate
+            zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+            zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+            zstyle ':completion:*' menu select
+            zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+            zstyle ':completion:*' verbose true
 
-      # use cache for completions
-      zstyle ':completion:*' use-cache on
-      zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
-      _comp_options+=(globdots)
+            # use cache for completions
+            zstyle ':completion:*' use-cache on
+            zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
+            _comp_options+=(globdots)
 
-      # Allow foot to pipe command output
-      function precmd {
-      if ! builtin zle; then
-      print -n "\e]133;D\e\\"
-      fi
-      }
+            # Allow foot to pipe command output
+            function precmd {
+            if ! builtin zle; then
+            print -n "\e]133;D\e\\"
+            fi
+            }
 
-      function preexec {
-      print -n "\e]133;C\e\\"
-      }
+            function preexec {
+            print -n "\e]133;C\e\\"
+            }
 
-      alias urlencode='python3 -c "import sys, urllib.parse as ul; \
-          print (ul.quote_plus(sys.argv[1]))"'
+            alias urlencode='python3 -c "import sys, urllib.parse as ul; \
+                print (ul.quote_plus(sys.argv[1]))"'
 
-      export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
-      export PATH=$PATH:''${NPM_GLOBAL:-~/.npm-global}/bin
-      export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin
+            export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
+            export PATH=$PATH:''${NPM_GLOBAL:-~/.npm-global}/bin
+            export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin
+
 
     '';
-
     shellAliases = {
       g = "git";
       grep = "grep --color";
