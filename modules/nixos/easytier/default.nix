ba47093a239217651bc76a29b6e52cf02b1f5fd4
zrubing <rubingem@gmail.com>
2026-02-01 00:12:35 +0800

    easytier

--- modules/nixos/easytier/default.nix
+++ modules/nixos/easytier/default.nix
@@ -15,28 +15,29 @@ in
   #   "${inputs.nixpkgs-unstable}/nixos/modules/services/networking/easytier.nix"
   # ];
 
-  # sops.templates."easytier-config".content = ''
-  #   instance_name = "zen14"
+  sops.templates."easytier-config".content = ''
+    instance_name = "zen14"
 
-  #   [network_identity]
-  #   network_name = "${config.sops.placeholder."easytier/ali/network_name"}"
-  #   network_secret = "${config.sops.placeholder."easytier/ali/network_secret"}"
+    [network_identity]
+    network_name = "${config.sops.placeholder."easytier/ali/network_name"}"
+    network_secret = "${config.sops.placeholder."easytier/ali/network_secret"}"
 
-  #   [[peer]]
-  #   uri = "${config.sops.placeholder."easytier/ali/peer"}"
-  # '';
+    [[peer]]
+    uri = "${config.sops.placeholder."easytier/ali/peer"}"
+  '';
 
-  # services.easytier = {
-  #   package = pkgs-unstable.easytier;
-  #   instances.easytier-net-zen14 = {
+  services.easytier = {
+    enable = true;
+    package = pkgs-unstable.easytier;
+    instances.easytier-net-zen14 = {
 
-  #     extraArgs = [
-  #       "-i"
-  #       "10.144.144.2"
-  #       "--accept-dns"
-  #       "true"
-  #     ];
-  #     configFile = "${config.sops.templates."easytier-config".path}";
-  #   };
-  # };
+      extraArgs = [
+        "-i"
+        "10.144.144.2"
+        "--accept-dns"
+        "true"
+      ];
+      configFile = "${config.sops.templates."easytier-config".path}";
+    };
+  };
 }
