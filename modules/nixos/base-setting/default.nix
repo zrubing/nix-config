6e0cf6f3585bcc6d83dc9625ad5423d20933f5cd
zrubing <rubingem@gmail.com>
2026-08-16 14:51:35 +0800

    certs: 信任 nova13 Caddy 本地 CA（dsh.local HTTPS 证书）

--- modules/nixos/base-setting/default.nix
+++ modules/nixos/base-setting/default.nix
@@ -2,6 +2,8 @@
 {
   security.pki.certificateFiles = [
     ../../../certs/ecc-ca.crt
+    # nova13 Caddy 本地 CA（tls internal）：信任后 dsh.local 的 HTTPS 证书才有效。
+    ../../../certs/caddy-local-nova13.crt
   ];
 
   # 26.05 默认切到 dbus-broker；实机升级时容易在 switch/boot 阶段触发已知兼容性问题。
